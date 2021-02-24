6ca5d56155eaacbc113ba8b7fd6c82e9f9c72ac2
Tezza27 <terryrees@hotmail.com>
2021-02-24 14:59:42 +0000

    Further Refactoring for Efficiency

--- lib/provider/colour.dart
+++ lib/provider/colour.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-// enum colourElements {
-//   redValue,
-//   greenValue,
-//   blueValue,
-//   opacityValue,
-// }
-
 class ColorUtility with ChangeNotifier {
   Color backgroundColor = Color.fromARGB(255, 0, 0, 0);
 
@@ -29,40 +22,7 @@ class ColorUtility with ChangeNotifier {
       case "blue":
         backgroundColor = backgroundColor.withBlue(value);
         break;
-        notifyListeners();
     }
+    notifyListeners();
   }
-
-  // int _redValue = 125;
-  // int _greenValue = 125;
-  // int _blueValue = 125;
-  // int _opacityValue = 255;
-
-  // int get redValue => _redValue;
-
-  // int get greenValue => _greenValue;
-
-  // int get blueValue => _blueValue;
-
-  // int get opacityValue => _opacityValue;
-
-  // set newOpacityValue(int newOpacity) {
-  //   _opacityValue = newOpacity;
-  //   notifyListeners();
-  // }
-
-  // set newRedValue(int newRed) {
-  //   _redValue = newRed;
-  //   notifyListeners();
-  // }
-
-  // set newGreenValue(int newGreen) {
-  //   _greenValue = newGreen;
-  //   notifyListeners();
-  // }
-
-  // set newBlueValue(int newBlue) {
-  //   _blueValue = newBlue;
-  //   notifyListeners();
-  // }
 }
